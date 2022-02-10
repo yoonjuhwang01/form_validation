82d21fdba114faff06201ffadd87edb03d367875
Yoonju Hwang <hwyj3464@gmail.com>
2022-02-10 15:52:51 -0500

    update index

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const name = documnet.getElementById('name');
-const password = documnet.getElementById('password');
-const form = documnet.getElementById('form');
+const name = documnet.getElementById('name')
+const password = documnet.getElementById('password')
+const form = documnet.getElementById('form')
+const errorElement = document.getElementById('error')
+
+
+form.addEventListener('submit', (e) => {
+    let messages=[]
+    if (name.value === '' || name.value == null){
+        messages.push('Name is required')
+    }
+
+    if (messages.length>0){
+        e.preventDefault()
+        errorElement.innerText = message.join(', ')
+
+    }
+    e.preventDefault()
+})
